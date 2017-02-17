66de0bd9bd4251b299273e8cf9d6113256734bdf
Laurence Chen <humorless@gmail.com>
2017-02-17 10:24:57 +0000

    add full emitAlert example and Alerta Docker config

--- emitAlert.py
+++ emitAlert.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
 import requests
 import time
@@ -8,17 +9,36 @@ import json
 url = "http://10.20.30.40:8181/api/alert"
 
 event="fastmedia.worker.process > 90"
+value="99.5"
 resource="gwb-sh-140.112.18.71"
-origin="owl"
+text="央视直播 is down"
+severity="major"
 status="open"
+attributes={"idc":"重庆联通-西南数据中心","platform":"c01.i07","contact":"蔺大奶","phone":"158000000000"}
+createTime="2017-02-07T08:57:23.654Z"
+origin="owl"
+correlate=["fastmedia.worker.process > 90", "fastmedia.worker.process > 95"]
+rawData="All the other details"
 d = {
     "event": event,
+    "value": value,
     "resource": resource,
-    "environment": "Production",
-    "service": ["none"],
+    "text": text,
+    "severity": severity,
+    "status": status,
+    "attributes": attributes,
+    "createTime": createTime,
     "origin": origin,
-    "status": status
+    "correlate": correlate,
+    "rawData": rawData
 }
 
 r = requests.post(url, data=json.dumps(d))
+print r.text
+
+
+
+d2 = d
+d2["event"]="fastmedia.worker.process > 95"
+r = requests.post(url, data=json.dumps(d2))
 print r.text
