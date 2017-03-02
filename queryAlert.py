a317d60a8d7f2be87ecf48a44e710e3a6c13c545
Laurence Chen <humorless@gmail.com>
2017-03-02 10:50:47 +0000

    modify queryAlert.py

--- queryAlert.py
+++ queryAlert.py
@@ -14,11 +14,14 @@ pagniation: Use limit to restrict how many items can stay in one page.
 apiKey = 'nskKwHlzEIuTejKqcYKT7-IB_owdw7Dd8sGhXceA'
 headers={'Authorization':'Key '+ apiKey}
 
-url = "http://10.20.30.40:8181/api/alerts"
-payload = {"limit":5,
+url = "http://alert.owl.fastweb.com.cn/api/alerts?from-date=2017-02-24T00:00:00.000Z"
+#url = "http://10.20.30.40:8181/api/alerts"
+payload = {"limit":20,
            "status":"open",
-           "page":77,
-           "origin":"NQM"}
+           "page":1,
+           "origin":"NQM",
+           "sort-by":"attributes.ping",
+           "reverse": True}
 r = requests.get(url, params=payload, headers=headers)
 print r.text
 
