eb92c7c9af5a6d6713f9b4f56c1ba1f69a8397bd
root <root@bgp-bj-124-243-198-162.cdn.fastweb.com.cn>
2017-08-18 16:40:36 +0800

    update /data to /ssd

--- backup_mongo.sh
+++ backup_mongo.sh
@@ -5,13 +5,13 @@ PW=xxxxxxx
 function backup_upload() {
   echo "execute backup "
   DATE=$(date +"%Y%m%d_%H%M%S")
-  tar zcf /data/backup_file/alerta_backup_"$DATE".tar.gz /data/backup 
+  tar zcf /ssd/backup_file/alerta_backup_"$DATE".tar.gz /ssd/backup 
   echo "execute upload "
-  curl -v -F "file=@/data/backup_file/alerta_backup_$DATE.tar.gz" -u $ID:$PW "http://ftp.owl.fastweb.com.cn/OWL-backup/mongo/?upload"
-  rm /data/backup_file/alerta_backup_"$DATE".tar.gz
+  curl -v -F "file=@/ssd/backup_file/alerta_backup_$DATE.tar.gz" -u $ID:$PW "http://ftp.owl.fastweb.com.cn/OWL-backup/mongo/?upload"
+  rm /ssd/backup_file/alerta_backup_"$DATE".tar.gz
 }
 
-YAML=/root/alerta_client/backup.yml
+YAML=./backup.yml
 # backup command
 docker-compose -f $YAML up -d backup-db
 
